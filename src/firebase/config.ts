4e1fa6f30cd36f2031d23fdd9cdebdffde8a52c9
Anjan Shomodder <thatanjan@gmail.com>
2024-11-30 20:49:42 +0600

    Setup is done with Firebase, Tailwind CSS, and DaisyUI Squashed commit of the following:
    commit 3d7faf359d1995c827777550a1f32d9f90df6bdc
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Sat Nov 30 20:42:47 2024 +0600
    
        Export db variable for firebase
    
    commit ed05826d4ba0e47400168b23691309b457b5bdf3
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Sat Nov 30 20:39:32 2024 +0600
    
        Convert firebase config to typescript
    
    commit 9713517d2f7fc425a76125731df99b6bab3598a2
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Sat Nov 30 20:38:10 2024 +0600
    
        Setup daisyui
    
    commit 7bb39d18bd54fe1b387a5e09b1659c29145c8160
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Sat Nov 30 20:28:16 2024 +0600
    
        Setup tailwind css
    
    commit dc05fa5b0e9fac1d6c2ccb194ea61a9c2afd4206
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Fri Nov 29 23:26:29 2024 +0600
    
        Added a simple change
    
    commit 259f93afb2559409bb2e3fc1175bf29590ee1e5b
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Fri Nov 29 23:25:38 2024 +0600
    
        Remove boilerplate css
    
    commit 074f6ac809e25132c3f4950e01d1decd7e1dc504
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Fri Nov 29 22:57:08 2024 +0600
    
        Add firebase config file
    
    commit e050fc4a8b75be12fd8b07bde4b2c41455e02b92
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Fri Nov 29 22:56:51 2024 +0600
    
        Install firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.WXT_FIREBASE_APIKEY,
+  authDomain: import.meta.env.WXT_FIREBASE_AUTHDOMAIN,
+  projectid: import.meta.env.WXT_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.WXT_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.WXT_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.WXT_FIREBASE_APPID,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export { db }
+
+export default app
